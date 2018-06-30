768f7aa61ffc5220f50ee095a49074afe49b5fc4
ST-8908 <seid.tab@gmail.com>
2018-06-30 13:31:16 +0200

    JWT token added

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://admin:admin123@ds161790.mlab.com:61790/dev_connect"
+  mongoURI: "mongodb://admin:admin123@ds161790.mlab.com:61790/dev_connect",
+  secretOrKey: "ThisIsMySuperSecret"
 };
