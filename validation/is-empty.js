66fb7c4d39836f3508094a30b9a718dd4313ea90
ST-8908 <seid.tab@gmail.com>
2018-07-02 15:30:23 +0200

    Added handle & user_id route + validation

--- validation/is-empty.js
+++ validation/is-empty.js
@@ -1,3 +1,7 @@
+/**
+ * La fonction "is-empty" de validator vérifie uniquement sur les Strings pas les objets ou autes types. Cette fonction Permet de vérifer si les valeurs exites de objets, strings ou null/undefined.
+ */
+
 const isEmpty = value =>
   value === undefined ||
   value === null ||
