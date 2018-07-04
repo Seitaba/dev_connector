b5cce3867e69b1b0a751e955b530058a65474a2f
ST-8908 <seid.tab@gmail.com>
2018-07-04 11:45:03 +0200

    Delete route added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -10,7 +10,7 @@ const validateEducationInput = require("../../validation/education");
 
 // Load Profile Model
 const Profile = require("../../models/Profile");
-// Load User Profile
+// Load User Model
 const User = require("../../models/User");
 
 // @route   GET api/profile/test
@@ -237,7 +237,7 @@ router.post(
 );
 
 // @route   DELETE api/profile/experience/:exp_id
-// @desc    Deletes experience from profile
+// @desc    Delete experience from profile
 // @access  Private
 router.delete(
   "/experience/:exp_id",
@@ -261,7 +261,7 @@ router.delete(
 );
 
 // @route   DELETE api/profile/education/:edu_id
-// @desc    Deletes education from profile
+// @desc    Delete education from profile
 // @access  Private
 router.delete(
   "/education/:edu_id",
@@ -281,6 +281,21 @@ router.delete(
         profile.save().then(profile => res.json(profile));
       })
       .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   DELETE api/profile
+// @desc    Delete user & profile
+// @access  Private
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findOneAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ success: true })
+      );
+    });
   }
 );
 
